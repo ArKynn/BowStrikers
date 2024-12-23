419afc45e87f4bc801b29eb1a5786e95971c18c5
ArKynn <david.f.mendes@proton.me>
2024-12-23 14:26:44 +0000

    Added options menu

--- Assets/Scripts/UI/UiManager.cs
+++ Assets/Scripts/UI/UiManager.cs
@@ -1,28 +1,53 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingsMenu;
     [SerializeField] private GameObject confirmMenu;
     [SerializeField] private GameObject splashScreen;
 
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
     [SerializeField] private GameObject turnUI;
     [SerializeField] private GameObject controlsUI;
     [SerializeField] private GameObject gameOverUI;
     [SerializeField] private TMP_Text _gameOverText;
     [SerializeField] private GameObject MainMenuFight;
     
+    private Resolution[] resolutions;
     private UIController uiController;
     private TMP_Text _turnText;
     private PlayerControlsUI _controlsController;
     private int activePlayerIndex;
+
+    private int FullScreen
+    {
+        get => PlayerPrefs.GetInt("FullScreen", 1);
+        set
+        {
+            PlayerPrefs.SetInt("FullScreen", value);
+            Screen.fullScreen = FullScreen == 1;
+        }
+    }
 
     void Start()
     {
         uiController = GetComponent<UIController>();
         _turnText = turnUI.GetComponent<TMP_Text>();
         _controlsController = controlsUI.GetComponent<PlayerControlsUI>();
+        UpdateResolutionsDropdown();
+    }
+
+    private void UpdateResolutionsDropdown()
+    {
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+        foreach (Resolution resolution in resolutions)
+        {
+            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData(resolution.ToString()));
+        }
     }
 
     public void StartGameButton()
@@ -97,5 +122,29 @@ public class UiManager : MonoBehaviour
         mainMenu.gameObject.SetActive(true);
         MainMenuFight.SetActive(false);
         uiController.GetEnabledButtons();
+    }
+
+    public void OpenOptionsMenu()
+    {
+        settingsMenu.gameObject.SetActive(true);
+        mainMenu.gameObject.SetActive(false);
+        uiController.GetEnabledButtons();
+    }
+
+    public void CloseOptionsMenu()
+    {
+        settingsMenu.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+        uiController.GetEnabledButtons();
+    }
+
+    public void ToggleFullScreen()
+    {
+        FullScreen = 1 - FullScreen;
+    }
+
+    public void SetResolution()
+    {
+        print(resolutionDropdown.options[resolutionDropdown.value].text);
     }
 }
